389ce4bbdfa7ad29d344acc0e9253571383e5334
Shivam Maurya <124660245+Shivamax-dev@users.noreply.github.com>
2026-02-24 18:10:05 +0000

    Datatypes in JS

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version of JS
+
+//alert (3 + 3) //we are using node js not browser so it gives error
+
+let name = "Shivam"
+let age = 20
+let isloggedin = true
+let state;
+
+//number => 2 to the power 20
+//bigint
+//string => True/False
+// Null => Standalone value
+//undefined
+//symbol => used for mostly for uniqueness
+//object
+ 
+console.log(typeof "Shivam");
+console.log(typeof age);
+console.log(typeof Null);
+console.log(typeof undefined);
+console.log(typeof state);
